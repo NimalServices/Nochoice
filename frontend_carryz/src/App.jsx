701f93814bd45406d9d1f441d5f084eedff30321
Rathinarasa Nimal <rathinarasanimal@gmail.com>
2026-03-18 23:36:09 +0530

    route and sign page demo created

--- frontend_carryz/src/App.jsx
+++ frontend_carryz/src/App.jsx
@@ -1,11 +1,19 @@
+import { createBrowserRouter,RouterProvider } from "react-router-dom";
+
 import Login from "./Pages/Login"
+import SignIn from "./Pages/SignIn"
 
 function App() {
-  
+  const router = createBrowserRouter([
+    { path: "/", element: <Login /> },
+    { path: "/signin", element: <SignIn /> }
+  ]);
+
 
   return (
     <>
-      <Login />
+      <RouterProvider router={router} />
+
     </>
   )
 }
--- frontend_carryz/src/Pages/Login.jsx
+++ frontend_carryz/src/Pages/Login.jsx
@@ -5,14 +5,15 @@ import styles from "../css/Login.module.css";
 function Login() {
 
   return (
-    <>
-    <h2>LogIn</h2>
+    <div className={styles.loginBackground}>
+    <h2>Carryz</h2>
+    <h5>Welcome back!</h5>
         <form className={styles.formLogin}>
             <input type="email" placeholder="Email" required />
             <input type="password" placeholder="Password" required />
             <input type="submit" value="Login" />
         </form>
-    </>
+    </div>
   );
 }
 
