57798f1fe7b95b8d037f4f6329004444a950a924
anbanshayan <anbanshayan3@gmail.com>
2026-04-06 21:44:51 +0530

    frontend changes

--- frontend_carryz/src/App.jsx
+++ frontend_carryz/src/App.jsx
@@ -2,11 +2,15 @@ import { createBrowserRouter,RouterProvider } from "react-router-dom";
 
 import Login from "./Pages/Login"
 import SignIn from "./Pages/SignIn"
+import Carrier from "./Pages/Carrier"
+import SearchCarrier from "./Pages/SearchCarrier"
 
 function App() {
   const router = createBrowserRouter([
     { path: "/", element: <Login /> },
-    { path: "/signin", element: <SignIn /> }
+    { path: "/signin", element: <SignIn /> },
+    { path: "/carrier", element: <Carrier /> },
+    { path: "/search-carrier", element: <SearchCarrier /> }
   ]);
 
 
