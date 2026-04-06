3132e7a94a0c9fd3362057bff5a7f24471a8c5b7
Rathinarasa Nimal <rathinarasanimal@gmail.com>
2026-04-06 09:56:45 +0530

    frontend and backend login okay

--- frontend_carryz/src/App.jsx
+++ frontend_carryz/src/App.jsx
@@ -1,12 +1,14 @@
 import { createBrowserRouter,RouterProvider } from "react-router-dom";
-
+import {BrowserRouter, Routes, Route} from "react-router-dom";
 import Login from "./Pages/Login"
 import SignIn from "./Pages/SignIn"
+import DashboardCareer from "./Pages/DashboardCareer";
 
 function App() {
   const router = createBrowserRouter([
     { path: "/", element: <Login /> },
-    { path: "/signin", element: <SignIn /> }
+    { path: "/signin", element: <SignIn /> },
+    { path: "/home",element: <DashboardCareer/>}
   ]);
 
 
---
+++ frontend_carryz/src/Pages/DashboardCareer.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const DashboardCareer = () => {
+  return (
+    <div>DashboardCareer</div>
+  )
+}
+
+export default DashboardCareer
